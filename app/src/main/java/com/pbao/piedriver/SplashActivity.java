cc3161647a6b8b5b00cbf8beb327c1de2f19a2a7
b4ro711 <baobaoo5533@gmail.com>
2023-08-21 23:25:01 +0700

    làm ẩn đi màu của status bar

--- app/src/main/java/com/pbao/piedriver/SplashActivity.java
+++ app/src/main/java/com/pbao/piedriver/SplashActivity.java
@@ -3,8 +3,13 @@ package com.pbao.piedriver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -19,6 +24,40 @@ public class SplashActivity extends AppCompatActivity {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        },2000);
+        }, 2000);
+
+        transparentStatusbar();
+    }
+
+    private void transparentStatusbar() {
+        if (Build.VERSION.SDK_INT >= 28 && Build.VERSION.SDK_INT < 34) {
+            setWidowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, true);
+        }
+        if (Build.VERSION.SDK_INT >= 28) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            );
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWidowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    private void setWidowFlag(int i, boolean b) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (b) {
+            winParams.flags |= i;
+        } else {
+            winParams.flags &= ~i;
+        }
+        win.setAttributes(winParams);
     }
 }
